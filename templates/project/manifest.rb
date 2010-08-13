f9c008459b706b108bf2ccd1d55aeedeae92dac9
Peter Gumeson <gumeson@gmail.com>
2010-08-12 17:02:09 -0700

    Added manifest and some default templates

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,26 @@
-# Make sure you list all the project template files here in the manifest.
-stylesheet 'screen.sass', :media => 'screen, projection'
+description "Compass extention for Html5 Boilerplate located at http://html5boilerplate.com"
+
+stylesheet 'screen.scss', :media => 'screen, projection'
+stylesheet 'handheld.scss', :media => 'handheld'
+stylesheet 'partials/_base.scss'
+stylesheet 'partials/_fonts.scss'
+stylesheet 'partials/_handheld.scss'
+stylesheet 'partials/_helpers.scss'
+stylesheet 'partials/_media.scss'
+stylesheet 'partials/_reset.scss'
+
+file 'README.rdoc'
+
+help %Q{
+Please see the Html5 Boilerplate website for more details:
+
+    http://html5boilerplate.com
+}
+
+welcome_message %Q{
+Please see the Html5 Boilerplate website for more details:
+
+    http://html5boilerplate.com
+    
+To get started, look inside screen.scss
+}
